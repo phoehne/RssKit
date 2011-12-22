d948f8aba4789cc2efcd29afc14fe17008027aa5
Paul Hoehne <phoehne@MacBook-Pro.local>
2011-12-22 16:17:27 -0500

    Initial check in.

---
+++ RssKitTests/TestRssLocator.m
@@ -0,0 +1,20 @@
+//
+//  TestRssLocator.m
+//  RssKit
+//
+//  Created by Paul Hoehne on 12/22/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "TestRssLocator.h"
+#import "RssLocator.h"
+
+@implementation TestRssLocator
+
+// All code under test must be linked into the Unit Test bundle
+- (void)testMath
+{
+    STAssertTrue((1 + 1) == 2, @"Compiler isn't feeling well today :-(");
+}
+
+@end
